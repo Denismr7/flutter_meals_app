0d6c8e2cf2ae2ed5319718a9d684159c0963871a
Denis Mory <denismr7@gmail.com>
2021-06-10 18:43:05 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: MealsApp(),
+  ));
+}
+
+class MealsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
